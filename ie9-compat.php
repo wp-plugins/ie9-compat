66aa22bf01ae7da917c5a40d66a67d0d13c6dc3a
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-11 08:51:47 +0000

    readme fix
    git-svn-id: https://plugins.svn.wordpress.org/ie9-compat/trunk@395893 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ie9-compat.php
+++ ie9-compat.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: IE9 Compat Inspector
-Plugin URI: http://www.ramoonus.nl
+Plugin URI: http://www.ramoonus.nl/wordpress/ie9-compat/
 Description: Compat Inspector is designed to help sites migrating to IE9 Standards Mode, not those running in legacy modes.
 Version: 1.0.0
 Author: Ramoonus
-Author URI: http://www.ramoonus.nl/wordpress/ie9-compat/
+Author URI: http://www.ramoonus.nl/
 */
 // source http://blogs.msdn.com/b/ie/archive/2011/04/27/ie9-compat-inspector.aspx
 // init
